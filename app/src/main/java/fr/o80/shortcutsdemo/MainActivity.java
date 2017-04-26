fbe56fecd35e03e085111912294d3343b25e28ae
Olivier PEREZ <olivier@olivierperez.fr>
2017-04-26 21:32:37 +0200

    Start

---
+++ app/src/main/java/fr/o80/shortcutsdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package fr.o80.shortcutsdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
+    }
+}
